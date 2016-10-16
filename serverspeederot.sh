51650ad6fbad4ecde871b4f577c895a48648e920
JulySnow <603723963@qq.com>
2016-10-16 09:55:35 +0800

    添加了一种新的情况 修复最后的Bug

--- serverspeederot.sh
+++ serverspeederot.sh
@@ -3,6 +3,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 clear
+if [ ! -d "/serverspeeder" ]; then
+  echo "You didn't Install ServerSpeeder"
+  echo "Please Install ..."
+fi
+
+clear
 echo
 echo "#############################################################"
 echo "# ServerSpeeder Optimization 1.0 For 91Yun ServerSpeeder    #"
@@ -61,7 +67,6 @@ Get_Dist_Name
 Get_OS_Bit
 Get_Kernel_Version
 Check_OpenVZ
-# 外网获取IP
 IP=`curl -s http://members.3322.org/dyndns/getip`;
 echo "IP is $IP"
 echo "**** System Infomation End****"
@@ -69,25 +74,23 @@ echo "**** System Infomation End****"
 echo "System information is correct? The correct return can not correctly please Ctrl+C stop"
 read -p 
 
-# 目前先这么放着 下面给他强制安装一下ppp套件
- apt-get install pptpd
- # 这里不写系统判断了 
- yum install -y ppp
+if [ "$DISTRO" == "CentOS" ]; then
+	# CentOS
+	yum install -y ppp
+	fi
+	if [[ "$DISTRO" == "Ubuntu" ]] || [[ "$DISTRO" == "Debian" ]]; then
+	# Debian/Ubuntu
+	apt-get install pptpd
+	fi
+
 cp /serverspeeder/etc/config /serverspeeder/etc/config.bak
- sed -i "s/accppp=\"0\"/accppp=\"1\"/g" /serverspeeder/etc/config
-
-# 高级入向加速
+sed -i "s/accppp=\"0\"/accppp=\"1\"/g" /serverspeeder/etc/config
 sed -i "s/advinacc="0"\"/advinacc=\"1\"/g" /serverspeeder/etc/config
-
-# 局域网加速 这个有点小必要
 sed -i "s/subnetAcc="0"\"/subnetAcc=\"1\"/g" /serverspeeder/etc/config
-
-# 加速时最高占用的内存以达到效果(极力推荐)
 clear
 echo "MEMINFO:"
 echo 
 free -m
-
 echo "Please Input Mem Free:"
 echo "For Exmple:"
 echo "IF YOUR MEMINFO just like this:"
@@ -98,20 +101,13 @@ Mem:          1002        560        442          0        144        212
 echo "$meminfo233"
 echo "Mem free is 560"
 echo "SO , Here in put 550"
-
 memshiji="512"
-read -p "InPUT MEM FREE(default 512): " memshiji
-
-memserverspeeder2=`echo "$memshiji * 8" | bc`
+read -p "Input MEM FREE(default 512): " memshiji
+memserverspeeder2=$[$memshiji*8]
 memserverspeederall=`echo $memshiji $memserverspeeder2`
-
 sed -i "s/l2wQLimit="256 2048"\"/l2wQLimit=\"$memserverspeederall\"/g" /serverspeeder/etc/config
 sed -i "s/w2lQLimit="256 2048"\"/w2lQLimit=\"$memserverspeederall\"/g" /serverspeeder/etc/config
-
-# 整体速度
-sed -i "s/initialCwndWan="22"\"/initialCwndWan=\"600\"/g" /serverspeeder/etc/config
-
-# 修改完后 保存修改
+sed -i "s/initialCwndWan="22"\"/initialCwndWan=\"650\"/g" /serverspeeder/etc/config
 bash /serverspeeder/bin/serverSpeeder.sh reload
 bash /serverspeeder/bin/serverSpeeder.sh restart
 
